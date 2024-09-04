f007ec94e5a04126f88ad0e16fb384b479ec0e62
Mohd Atif Ansari <114794817+Atif468@users.noreply.github.com>
2024-09-05 01:26:54 +0530

    Add files via upload
    add files

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import path from "path";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
@@ -32,19 +34,21 @@ const userSchema = mongoose.Schema({
 
 const userModel = mongoose.model("userModel", userSchema);
 
-const sendtodb = (data) => {
-    userModel.create({
+const sendtodb = async (data) => {
+    const dta = await userModel.create({
         user: data.name,
         email: data.email,
     }).then(() => {
         console.log("successfully send to db");
+        return dta;
     }).catch((error) => {
         console.log("error in sending to db");
     })
 }
+
 const checklogin = (req, res, next) => {
     console.log("cookie check");
-    const cook = req.cookies.user;
+    const cook = req.cookies.token;
     console.log(cook);
     if( cook )
     {
@@ -65,7 +69,7 @@ app.get("/",  checklogin, (req, res) => {
 
 app.get("/logout", (req, res) => {
 
-    res.clearCookie("user");
+    res.clearCookie("token");
     console.log("logout");
     res.redirect("/");
 })
@@ -74,10 +78,30 @@ app.get("/success", (req, res) => {
     res.render("success");
 })
 
-app.post("/login", (req, res) => {
-    sendtodb(req.body);
+app.post("/register", async (req, res) => {
+
+    const data = await sendtodb(req.body);
     console.log(req.body);
-    res.cookie("user", req.body.name);
+    const token = jwt.sign({data}, "jhjbh");
+
+    res.cookie("token", token);
+    res.redirect("/login");
+})
+
+app.post("/login", (req, res) => {
+    // const data = sendtodb(req.body);
+    // console.log(req.body);
+    
+    // res.cookie("token", token);
+    const {name, email} = req.body;
+    const token = jwt.verify({}, "jhjbh");
+
+    const result = User.findOne({email});
+    if(!result)
+    {
+        alert("register first");
+        res.render("register");
+    }
     res.redirect("/success");
 })
 
